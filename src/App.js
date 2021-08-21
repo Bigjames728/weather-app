29a08294520caf644d6c6c61d2348afe3d22954b
Bigjames728 <bigjames728@gmail.com>
2021-08-20 18:12:13 -0700

    Set api to object with the api key and url

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+const API = {
+  key: "1b8dd4b36d114e8798839fe9fab911a7",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <main>
+       <div className="search-box">
+         <input type="text" className="search-bar" placeholder="Search.." />
+       </div> 
+      </main>
     </div>
   );
 }
